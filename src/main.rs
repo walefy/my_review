8f8964e42e63d3297ef20995cae9319bf748a365
walefy <walefyd1@gmail.com>
2024-08-29 14:59:16 -0300

    chore: add serde dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[derive(serde::Serialize)]
+enum HealthStatus {
+    #[serde(rename = "healthy")]
+    Health,
+}
+
+#[derive(serde::Serialize)]
+struct HealthResponse {
+    status: HealthStatus,
+}
+
+#[get("/")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().json(HealthResponse {
+        status: HealthStatus::Health,
+    })
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health_check))
+        .bind(("localhost", 3001))?
+        .run()
+        .await
 }
