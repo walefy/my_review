d302942b720cb4989b7126ba2889fc7f9123e6e1
walefy <walefyd1@gmail.com>
2024-08-29 17:01:36 -0300

    feat: add prisma schema and client config

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,25 @@ mod controller;
 mod dto;
 mod enums;
 mod model;
+#[allow(warnings, unused)]
+mod prisma;
 mod routes;
 mod service;
 
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
+use prisma::*;
 use routes::health_router::health_router;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(health_router()))
+    let client = web::Data::new(
+        PrismaClient::_builder()
+            .build()
+            .await
+            .expect("can't connect to database!"),
+    );
+
+    HttpServer::new(move || App::new().app_data(client.clone()).service(health_router()))
         .bind(("localhost", 3001))?
         .run()
         .await
