a2be04f6c66793e8e91af2d88edadf9d274740ac
walefy <walefyd1@gmail.com>
2024-08-29 16:32:30 -0300

    chore: add prisma-client-rust

--- src/controller/health_controller.rs
+++ src/controller/health_controller.rs
@@ -1,4 +1,5 @@
-use crate::{dto::health_response::HealthResponse, enums::health_status::HealthStatus};
+use crate::dto::health_response::HealthResponse;
+use crate::enums::health_status::HealthStatus;
 use actix_web::{HttpResponse, Responder};
 
 pub async fn health_check() -> impl Responder {
