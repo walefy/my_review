8498ecfb2a218b87e08c86e8f0060a831d7fb403
walefy <walefyd1@gmail.com>
2024-09-02 22:31:05 -0300

    refactor: change resource to scope in health_route

--- src/controller/health_controller.rs
+++ src/controller/health_controller.rs
@@ -1,7 +1,8 @@
 use crate::dto::HealthResponse;
 use crate::enums::HealthStatus;
-use actix_web::{HttpResponse, Responder};
+use actix_web::{get, HttpResponse, Responder};
 
+#[get("")]
 pub async fn health_check() -> impl Responder {
     HttpResponse::Ok().json(HealthResponse {
         status: HealthStatus::Health,
--- src/routes/health_router.rs
+++ src/routes/health_router.rs
@@ -1,7 +1,7 @@
-use actix_web::{web, Resource};
+use actix_web::{web, Scope};
 
 use crate::controller::health_controller::health_check;
 
-pub fn health_router() -> Resource {
-    web::resource("/health").route(web::get().to(health_check))
+pub fn health_router() -> Scope {
+    web::scope("/health").service(health_check)
 }
