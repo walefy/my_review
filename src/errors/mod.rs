aa65d3c021bad135d68613112e84e350b14d3796
walefy <walefyd1@gmail.com>
2024-09-10 22:16:12 -0300

    refactor: change UserError to GenericError

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,7 +1,4 @@
 mod generic_error;
-mod user_error;
 
 pub use generic_error::ErrorPayload;
 pub use generic_error::GenericError;
-pub use user_error::UserError;
-pub use user_error::UserErrorPayload;
--- src/service/user_service.rs
+++ src/service/user_service.rs
@@ -1,5 +1,5 @@
 use crate::enums::HttpStatus;
-use crate::errors::{UserError, UserErrorPayload};
+use crate::errors::{ErrorPayload, GenericError};
 use crate::prisma::{user, PrismaClient};
 use actix_web::web::Data;
 
@@ -9,24 +9,24 @@ use crate::model::user_model;
 pub async fn create(
     user_creation: UserCreation,
     client: Data<PrismaClient>,
-) -> Result<user::Data, UserError> {
+) -> Result<user::Data, GenericError> {
     let user_found = user_model::find_user_by_email(user_creation.email.clone(), &client).await;
 
     match user_found {
         Ok(res) => {
             if res.is_some() {
-                return Err(UserError {
+                return Err(GenericError {
                     status: HttpStatus::Conflict,
-                    payload: UserErrorPayload {
+                    payload: ErrorPayload {
                         message: "user already exists!".to_string(),
                     },
                 });
             }
         }
         Err(_) => {
-            return Err(UserError {
+            return Err(GenericError {
                 status: HttpStatus::BadRequest,
-                payload: UserErrorPayload {
+                payload: ErrorPayload {
                     message: "can't create user".to_string(),
                 },
             })
@@ -37,39 +37,42 @@ pub async fn create(
 
     match result {
         Ok(model_response) => Ok(model_response),
-        Err(_) => Err(UserError {
+        Err(_) => Err(GenericError {
             status: HttpStatus::BadRequest,
             // TODO: fazer um mapeamento melhor de qual erro ocorreu
-            payload: UserErrorPayload {
+            payload: ErrorPayload {
                 message: "can't create user".to_string(),
             },
         }),
     }
 }
 
-pub async fn find_all_user(client: Data<PrismaClient>) -> Result<Vec<user::Data>, UserError> {
+pub async fn find_all_user(client: Data<PrismaClient>) -> Result<Vec<user::Data>, GenericError> {
     let result = user_model::find_all_user(&client).await;
 
     match result {
         Ok(users) => Ok(users),
-        Err(_) => Err(UserError {
+        Err(_) => Err(GenericError {
             status: HttpStatus::BadRequest,
-            payload: UserErrorPayload {
+            payload: ErrorPayload {
                 message: "can't find all users".to_string(),
             },
         }),
     }
 }
 
-pub async fn find_user_by_id(client: Data<PrismaClient>, id: i32) -> Result<user::Data, UserError> {
+pub async fn find_user_by_id(
+    client: Data<PrismaClient>,
+    id: i32,
+) -> Result<user::Data, GenericError> {
     let result = user_model::find_user_by_id(id, &client).await;
 
     match result {
         Ok(user) => {
             if user.is_none() {
-                return Err(UserError {
+                return Err(GenericError {
                     status: HttpStatus::NotFound,
-                    payload: UserErrorPayload {
+                    payload: ErrorPayload {
                         message: format!("can't find user with id {id}"),
                     },
                 });
@@ -77,9 +80,9 @@ pub async fn find_user_by_id(client: Data<PrismaClient>, id: i32) -> Result<user
 
             Ok(user.unwrap())
         }
-        Err(_) => Err(UserError {
+        Err(_) => Err(GenericError {
             status: HttpStatus::NotFound,
-            payload: UserErrorPayload {
+            payload: ErrorPayload {
                 message: format!("can't find user with id {id}"),
             },
         }),
@@ -89,24 +92,24 @@ pub async fn find_user_by_id(client: Data<PrismaClient>, id: i32) -> Result<user
 pub async fn delete_by_id(
     client: Data<PrismaClient>,
     id: i32,
-) -> Result<ServiceResponse, UserError> {
+) -> Result<ServiceResponse, GenericError> {
     let user_found = user_model::find_user_by_id(id, &client).await;
 
     match user_found {
         Ok(user) => {
             if user.is_none() {
-                return Err(UserError {
+                return Err(GenericError {
                     status: HttpStatus::NotFound,
-                    payload: UserErrorPayload {
+                    payload: ErrorPayload {
                         message: format!("can't find user with id {id}"),
                     },
                 });
             }
         }
         Err(_) => {
-            return Err(UserError {
+            return Err(GenericError {
                 status: HttpStatus::NotFound,
-                payload: UserErrorPayload {
+                payload: ErrorPayload {
                     message: format!("can't find user with id {id}"),
                 },
             });
@@ -114,8 +117,6 @@ pub async fn delete_by_id(
     }
 
     let result = user_model::delete_user_by_id(id, &client).await;
-
-    // TODO: migrar todas as funções da service para o modelo ServiceResponse (remover UserError)
     match result {
         Ok(_) => Ok(ServiceResponse {
             status: HttpStatus::NoContent,
@@ -123,9 +124,9 @@ pub async fn delete_by_id(
                 message: "user deleted!".to_string(),
             },
         }),
-        Err(_) => Err(UserError {
+        Err(_) => Err(GenericError {
             status: HttpStatus::BadRequest,
-            payload: UserErrorPayload {
+            payload: ErrorPayload {
                 message: "can't delete user!".to_string(),
             },
         }),
