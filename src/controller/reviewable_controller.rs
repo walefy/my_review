277e94ec8f65f0e2c5dc49383f4b495ef771774c
walefy <walefyd1@gmail.com>
2024-09-19 15:05:07 -0300

    feat: implements find all reviewable (controller)

--- src/controller/reviewable_controller.rs
+++ src/controller/reviewable_controller.rs
@@ -1,5 +1,5 @@
 use actix_web::web::{Data, Json};
-use actix_web::{post, HttpResponse, Responder};
+use actix_web::{get, post, HttpResponse, Responder};
 
 use crate::dto::ReviewableCreation;
 use crate::service::reviewable_service;
@@ -14,6 +14,19 @@ async fn create(client: Data<PrismaClient>, body: Json<ReviewableCreation>) -> i
         Err(service_err) => {
             let status = service_err.status;
 
+            status.to_response_builder().json(service_err.payload)
+        }
+    }
+}
+
+#[get("")]
+async fn find_all(client: Data<PrismaClient>) -> impl Responder {
+    let result = reviewable_service::find_all(client).await;
+
+    match result {
+        Ok(service_response) => HttpResponse::Ok().json(service_response),
+        Err(service_err) => {
+            let status = service_err.status;
             status.to_response_builder().json(service_err.payload)
         }
     }
